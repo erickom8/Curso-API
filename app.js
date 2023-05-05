14fb2d5fffbaa787ee6e843d32901280928eb776
erickom8_ <erick.cirico@gmail.com>
2023-05-04 22:30:01 -0300

    Conectando a API ao servidor

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));//Apenas dados simples
 app.use(bodyParser.json());//json de entrada no body
 
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');// Caso fosse em mum servidor específico, colar a url... (o '*' significa: TODOS)
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requerested-With,Content-Type, Accept, Authorization'
+        );
+
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Headers', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).send({});
+        }
+        next();
+    });
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
